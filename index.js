6dab6f05f676af5fd0b687a74dc81eed80b1f0f5
rahulsan007 <santrarahul7070@gmail.com>
2024-09-14 10:43:20 +0530

    basic command working

---
+++ index.js
@@ -0,0 +1,27 @@
+const net = require('net');
+const {parseCommand , executeCommand, init} = require('./core.js');
+const port = 6378;
+const host = "127.0.0.1";
+const server = net.createServer((connection) =>
+  {
+    console.log('Client connected');
+    connection.on ('data', (data) =>
+      {
+        let response
+        // console.log('==>' + data.toString());
+        try {
+        const {command, args} = parseCommand(data);
+         response = executeCommand(command, args);
+
+        } catch (error) {
+          response = "-Error unknown command\r\n"
+        }
+
+        connection.write(response);
+      });
+  });
+
+  server.listen(port, host, () => {
+    init();
+    console.log(`Redis Clone Server started at port ${port}`);
+  });
